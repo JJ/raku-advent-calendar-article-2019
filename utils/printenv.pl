0092cd635addfd74d02ad67df699e553c46865c4
JJ Merelo <jjmerelo@gmail.com>
2019-12-04 17:16:14 +0100

    Change output

--- utils/printenv.pl
+++ utils/printenv.pl
@@ -6,9 +6,5 @@ use warnings;
 use v5.16;
 
 my $output = ( ($ENV{BODY} =~ /Merry/) == 1)? $ENV{GREETING} : $ENV{HEY};
-my $json =<<EOF;
-'{ "body": "$output"}'
-EOF
-
-print $json;
+print "\'{ \"body\": \"$output\"}\"\'";
 

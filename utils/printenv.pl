87dfb7479479fcb1bcc48b7f4c9fa44f4f290b62
JJ Merelo <jjmerelo@gmail.com>
2019-12-04 14:35:04 +0100

    Prints out

---
+++ utils/printenv.pl
@@ -0,0 +1,16 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+
+use v5.16;
+
+my $output = ( ($ENV{BODY} =~ /Merry/) == 1)? $ENV{GREETING} : $ENV{HEY};
+my $json =<<EOF;
+{
+    "body": "$output"
+}
+EOF
+
+print $json;
+
